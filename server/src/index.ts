a54ae2a16a15d036632f4f338b5e60c521980b78
jyotiGoyal16 <goyaljyoti163@gmail.com>
2026-05-03 01:09:08 +0530

    Revert "fix: serve frontend from express to eliminate cross-origin cookie issue"
    This reverts commit aaad684266db58c240e04553915b89be616eab3b.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import path from "path";
 import { sessionMiddleWare } from "./middlewares/session";
 import authRouter from "./routes/auth";
 import mailRouter from "./routes/mail";
@@ -16,13 +15,6 @@ app.use(sessionMiddleWare);
 app.use("/auth", authRouter);
 app.use("/mail", mailRouter);
 app.use("/applications", applicationsRouter);
-
-// Serve React frontend
-const frontendBuildPath = path.join(__dirname, "../../dist");
-app.use(express.static(frontendBuildPath));
-app.get("*", (_req, res) => {
-  res.sendFile(path.join(frontendBuildPath, "index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
