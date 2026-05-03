1a474a7b57e4f870cbb0c30d21bf452f9967701a
jyotiGoyal16 <goyaljyoti163@gmail.com>
2026-05-03 08:33:30 +0530

    reverted vercel configs and updates

--- server/src/middlewares/session.ts
+++ server/src/middlewares/session.ts
@@ -23,7 +23,7 @@ export const sessionMiddleWare = [
     cookie: {
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
     },
   }),
